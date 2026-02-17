5614298387074db31d3c91f2037bda6063cd6790
jskakk <basejascha@gmail.com>
2026-02-17 21:08:35 +0800

    feature: complete all features in backend and frontend.

--- backend/src/main/java/com/studyspace/backend/dto/AuthRequest.java
+++ backend/src/main/java/com/studyspace/backend/dto/AuthRequest.java
@@ -1,13 +1,29 @@
 package com.studyspace.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AuthRequest {
     private String identifier;
     private String password;
+
+    public AuthRequest() {}
+
+    public AuthRequest(String identifier, String password) {
+        this.identifier = identifier;
+        this.password = password;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- backend/src/main/java/com/studyspace/backend/dto/AuthResponse.java
+++ backend/src/main/java/com/studyspace/backend/dto/AuthResponse.java
@@ -1,12 +1,39 @@
 package com.studyspace.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class AuthResponse {
     private Long id;
     private String username;
     private String email;
+
+    public AuthResponse() {}
+
+    public AuthResponse(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
